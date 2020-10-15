2e0b20e69406ceb3520b3c2e60fa04653b77717f
Paul Omondi <paulo.techie@gmail.com>
2020-10-15 23:54:29 +0300

    Add gameboard logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,6 @@ const createPlayer = (name, shape) => ({
     }
 });
 
-
-
 const display = (() => {
     const $form = document.querySelector('#player');
 
@@ -47,14 +45,14 @@ const display = (() => {
 })();
 
 const game = (() => {
-    let board = Array.from({ length: 9 }),
+    // let board = Array.from({ length: 9 }),
         player1 = null,
         player2 = null;
 
 
 
     async function init() {
-        board = Array.from({ length: 9 });
+        // board = Array.from({ length: 9 });
 
         let data = null;
         display.showPlayerForm('Player 1 details');
@@ -69,6 +67,73 @@ const game = (() => {
     return {
         init
     };
+})();
+
+const gameboard = (() => {
+
+    const gameCells = ["", "", "", "", "", "", "", "", ""];
+
+    const getGameCells = () => {
+        return gameCells;
+    }
+
+    const fillPlayedCell = (index) => {
+        if (gameCells[index] === "") {
+            gameCells[index] = getCurrentPlayer().getShape();
+            return true;
+        }
+        else { 
+        return false;
+        }
+    };
+
+    const checkWinner = (shape) => {
+        const winningCells = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+        const playerBoard = [];
+
+        for (let i=0; i<gameCells.length; i++) {
+            if (gameCells[i] === shape) {
+                playerBoard.push(i);
+            }
+        }
+
+        for (let item of winningCells) {
+            if (item.every(index => gameCells[index] === shape))
+                return 'win';
+        }
+
+        for (let i=0; i<gameCells.length; i++) {
+            if (gameCells[i] === "") {
+                return 'nowin'
+            }
+        }
+
+        return 'draw';
+        
+    }
+
+    const displayBoard = (() => {
+        const boardDiv = document.getElementById('#board');
+    
+    
+    
+        return {
+            showPlayerForm,
+            getPlayerFormData,
+            hidePlayerForm
+        };
+
+
 })();
 
 game.init().catch(console.log);
